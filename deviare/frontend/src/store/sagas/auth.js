48e2e36344cb173afa7e0df27f9c10ed8be60266
Thembelani <thembelani@humbletill.com>
2021-03-08 11:30:46 +0200

    login sagas error trapping

--- deviare/frontend/src/store/sagas/auth.js
+++ deviare/frontend/src/store/sagas/auth.js
@@ -8,41 +8,60 @@ import Redux from '../redux/exports';
 export const api = API.create();
 
 export function * loginSaga(action) {
-    const {username, password} = action;
+    try {
+        const {username, password} = action;
 
-    yield delay(4000);
-
-    const response = yield call(api.postLogin, username, password);
-    if (response.ok) {
-        const { user, token, refresh_token } = response.data;
-        yield put(Redux.AuthActions.loginSuccess(token, refresh_token, user));
-        yield put(push('/home'));
-    } else {
-        yield put(Redux.AuthActions.loginFailure(response.data, 'Login Failed'));
-        /** 
-         * Check for expired token, attempt refresh
-         * 
+        yield delay(4000);
+        const response = yield call(api.postLogin, username, password);
+        if (response.ok) {
+            const { user, token, refresh_token } = response.data;
+            yield put(Redux.AuthActions.loginSuccess(token, refresh_token, user));
+            yield put(push('/home'));
+        } else {
+            yield put(Redux.AuthActions.loginFailure(response.data, 'Login Failed'));
+            /** 
+             * Check for expired token, attempt refresh
+             * 
+             */
+            yield call(manageLoginAttemptsSaga);
+        }
+    } catch (e) {
+        /**
+         * Global error logging
          */
-        yield call(manageLoginAttemptsSaga);
     }
+
 }
 
 export function * logoutSaga () {
-    /**
-     * Does some Fancy Cleanup
-     */
-    yield delay(4000);
-     yield put(Redux.AuthActions.logoutSuccess());
-     yield put(push('/login'));
+    try {
+        /**
+         * Does some Fancy Cleanup
+         */
+        yield delay(4000);
+        yield put(Redux.AuthActions.logoutSuccess());
+        yield put(push('/login'));
+    } catch (e) {
+        /**
+         * Global error logging
+         */
+    }
+    
 }
 
 function * manageLoginAttemptsSaga () {
-    const attempts = yield select(Redux.AuthSelectors.selectAttempts());
-    const isLocked = yield select(Redux.AuthSelectors.selectLocked());
-    if (attempts >= 3 && !isLocked) {
-        yield put(Redux.AuthActions.isLocked(true));
-        yield delay(10000);
-        yield put(Redux.AuthActions.isLocked(false));
-        yield put(Redux.AuthActions.resetAttempts())
+    try {
+        const attempts = yield select(Redux.AuthSelectors.selectAttempts());
+        const isLocked = yield select(Redux.AuthSelectors.selectLocked());
+        if (attempts >= 3 && !isLocked) {
+            yield put(Redux.AuthActions.isLocked(true));
+            yield delay(10000);
+            yield put(Redux.AuthActions.isLocked(false));
+            yield put(Redux.AuthActions.resetAttempts())
+        }
+    } catch (e) {
+        /**
+         * Global error logging
+         */
     }
 }
